ef48b822d43ae4afd8e14400e40fab3d98f65fa7
Jose Toscano <josetoscano@atratopago.com>
2020-04-29 15:01:10 -0500

    adding lag-lead compensation controller for transfer functions

--- root_locus_and_Lag_Lead_controller.py
+++ root_locus_and_Lag_Lead_controller.py
@@ -20,18 +20,16 @@ den2=[1,2,3]
 num4=[1]
 den4=[1,6,11,6]
 num1=[1]
-den1=[1,3,2,0]
+den1=[1,5,6,0]
 num3=[1,3]
 den3=[1,7,14,8,0]
 num5=[1]
 den5=[1,3,2]
-num6=[2]
-den6=[1, 14, 48]
-num7=[33.6213]
-den7=[1,3.0035,11.2071]
+num6=[1]
+den6=[1, 0, -1]
 
-num=num4
-den=den4
+num=num6
+den=den6
 G = control.tf(num,den)
 H = 1
 G_feedbacked = control.feedback(G,H)
@@ -215,7 +213,7 @@ omega= control.phase_crossover_frequencies(G)
 pdf.set_font('arial', 'B', 12.0)
 pdf.cell(200, 10, "jW - axis crossing:", 0, 2, 'L')
 pdf.set_font('arial', '', 11.0)
-if omega[0][0] != 0:
+if (omega[0].any()) and (omega[0][0] != 0):
     s=omega[0][0]*1j
     power=0
     num_with_values = []
@@ -236,146 +234,99 @@ else:
     control.sisotool(G)
     print("There is no crossing of the jW-axis, review sisotool plot")
     pdf.cell(200, 10,"There is no crossing of the jW-axis, review sisotool plot", 0, 2, 'L')
-
+pdf.set_font('arial', 'B', 12.0)
+pdf.cell(200, 10, "Evaluating Test points:" + str(testing_points), 0, 2, 'L')
+pdf.set_font('arial', '', 11.0)
 for x in range(0,len(testing_points)):
-    #print("testing point is: " + str(poles[i-1]))
     testing_point=testing_points[x]
-    sum_of_poles_angles=0
-    sum_of_zeros_angles=0
-    for j in range(0,len(zeros)):
-        angle=0
-        x1 = np.real(testing_point)
-        y1 = np.imag(testing_point)
-        x2 = np.real(zeros[j])
-        y2 = np.imag(zeros[j])
-        if(x1==x2):
-            angle=90
-        if((x1==x2)and(y1==y2)):
-            angle=0
-        if (x1 != x2):
-            angle = math.degrees(math.atan((y2-y1)/(x2-x1)))
-            if ( x1 < x2 and angle==0):
-                angle = 180
-            if ( x2 < x1 and angle==0):
-                angle = 0
-        if angle < 0:
-            angle = 180-abs(angle) 
-        sum_of_zeros_angles += angle
-        #print("  angle for point" + str(zeros[j]) + " is " + str(angle))
-    #print("sum of zeros: " + str(sum_of_zeros_angles)    )
-    for k in range(0,len(poles)):
-        angle=0
-        x1 = np.real(testing_point)
-        y1 = np.imag(testing_point)
-        x2 = np.real(poles[k])
-        y2 = np.imag(poles[k])
-        if(x1==x2):
-            angle=90
-        if((x1==x2)and(y1==y2)):
-            angle=0
-        if (x1 != x2):
-            angle = math.degrees(math.atan((y2-y1)/(x2-x1)))
-            if ( x1 < x2 and angle==0):
-                angle = 180
-            if ( x2 < x1 and angle==0):
-                angle = 0
-        if angle < 0:
-            angle = 180-abs(angle) 
-        sum_of_poles_angles += angle
-        #print("  angle for point" + str(poles[k]) + " is " + str(angle))
-    #print("sum of poles: " + str(sum_of_poles_angles)    )
-    sum_angles_zeros = np.sum(sum_of_zeros_angles)
-    sum_angles_poles = np.sum(sum_of_poles_angles)
-    angle_of_testing_point = sum_angles_zeros - sum_angles_poles
-
-    if angle_of_testing_point < 0:
-        angle_of_testing_point += 360
-
-    print("Angle for testing point " + str(testing_point) + "is:" + str(angle_of_testing_point))
-    pdf.cell(200, 10,"Angle for testing point " + str(testing_point) + " is: " + str(angle_of_testing_point), 0, 2, 'L')
-    if (angle_of_testing_point/180)%2 == 1:
-        print("the point" + str(testing_point) + " does belong to the root loccus")
-        pdf.cell(200, 10,"the point" + str(testing_point) + " does belong to the root loccus", 0, 2, 'L')
-        num_evaluated=1
-        den_evaluated=1
-        for i in range(0,len(num)):
-            real_part = -np.real(num[i])+np.real(testing_point)
-            imag_part = -np.imag(num[i])+np.imag(testing_point)
-            sum_value = np.complex(real_part,imag_part)
-            num_evaluated *= sum_value
-        for i in range(0,len(den)):
-            real_part = -np.real(den[i])+np.real(testing_point)
-            imag_part = -np.imag(den[i])+np.imag(testing_point)
-            sum_value = np.complex(real_part,imag_part)
-            den_evaluated *= sum_value
-        response = control.evalfr(G, testing_point)
-        value_of_K_testing_point = round(1/abs(response),4)
-        magnitud_K = (1/abs(response))
-        print("The magnitud of K corresponding to the test point " + str(testing_point) + " is " + str(magnitud_K))
-        pdf.cell(200, 10,"The magnitud of K corresponding to the test point " + str(testing_point) + " is " + str(magnitud_K), 0, 2, 'L')
+    angle_G_evaluated_at_testpoint=math.degrees(np.angle(control.evalfr(G, testing_point)))
+    print("angle for test point: " + str(testing_point) + " is "+ str(angle_G_evaluated_at_testpoint))
+    pdf.cell(200, 10,"angle for test point: " + str(testing_point) + " is "+ str(angle_G_evaluated_at_testpoint), 0, 2, 'L')
+    if (((angle_G_evaluated_at_testpoint/180)%2)!= 0 and (angle_G_evaluated_at_testpoint%180 == 0)):
+        print("This test point does belong to the root loccus")
+        pdf.cell(200, 10,"This test point does belong to the root loccus", 0, 2, 'L')
+        #find value of K for this testing point
+        plant_value = control.evalfr(G, testing_point)
+        value_of_K_plant = 1/abs(plant_value)
+        print("the value of K corresponding to this testing point is " + str(value_of_K_plant))
+        pdf.cell(200, 10,"the value of K corresponding to this testing point is " + str(value_of_K_plant), 0, 2, 'L')
     else:
-        #print((angle_of_testing_point/180)%1)
-        angle_defficit = 180-((angle_of_testing_point/180)%1 * 180)
-        print("the point" + str(testing_point) + " does not belong to the root loccus. The angle deficit is: " + str(angle_defficit) + " degrees, or " + str(round(math.radians(angle_defficit),4)) + " radians.")
-        
-        #Lead Controler Design
-
-        pdf.set_font('arial', 'B', 12.0)
-        pdf.cell(200, 10, "Lead Controler Design:", 0, 2, 'L')
-        pdf.set_font('arial', '', 11.0)
-        pdf.cell(200, 10,"the point" + str(testing_point) + " does not belong to the root loccus. The angle deficit is: " + str(angle_defficit) + " degrees, or " + str(round(math.radians(angle_defficit),4)) + " radians.", 0, 2, 'L')
-        new_zero = np.real(testing_point)
-        print("The zero of the Lead Controler is in S=" +str(new_zero))
-        pdf.cell(200, 10,"The zero of the Lead Controler is in S=" +str(new_zero), 0, 2, 'L')
-        angle_new_pole= 90 - angle_defficit
-        new_pole = 0
-        print(angle_defficit)
-        if(abs(90-angle_defficit)>0.01):
-            distance_pole_zero = (np.imag(testing_point)/math.tan(math.radians(angle_new_pole)))
-            new_pole = new_zero - distance_pole_zero
-            print("The pole of the Lead Controler is in S=" +str(new_pole))
-            pdf.cell(200, 10,"The pole of the Lead Controler is in S=" +str(new_pole), 0, 2, 'L')
-        else:
-            print("There is no need of an extra pole")
-            pdf.cell(200, 10,"There is no need of an extra pole", 0, 2, 'L')
-        #evaluate G*Controler and find magnitude
-        num_evaluated=1
-        den_evaluated=1
-        if (len(zeros) == 0):
-            zeros = [1]
-        if (len(poles) == 0):
-            poles = [1]
-        num_and_controler_zero = np.concatenate([zeros, [new_zero]])
-        den_and_controler_pole = np.concatenate([poles, [new_pole]])
-        for i in range(0,len(num_and_controler_zero)):
-            real_part = -np.real(num_and_controler_zero[i])+np.real(testing_point)
-            imag_part = -np.imag(num_and_controler_zero[i])+np.imag(testing_point)
-            sum_value = np.complex(real_part,imag_part)
-            num_evaluated *= sum_value
-        for i in range(0,len(den_and_controler_pole)):
-            real_part = -np.real(den_and_controler_pole[i])+np.real(testing_point)
-            imag_part = -np.imag(den_and_controler_pole[i])+np.imag(testing_point)
-            sum_value = np.complex(real_part,imag_part)
-            den_evaluated *= sum_value
-        G_evaluated_testpoint = num_evaluated/den_evaluated        
-        print("G directa con metodo:", str(control.evalfr(G, (-2+2j))))
-        print("angulo de G directa con metodo:", str(math.degrees(np.angle(control.evalfr(G, (-2+2j))))))
-        controller = control.tf([1, -new_zero],[1,-new_pole])
-        if (new_zero == 0):
-            controller = control.tf([1],[1,-new_pole])
-        if (new_pole == 0):
-            controller = control.tf([1, -new_zero],[1])
-        print(controller)
-        lead_controller = control.series(G,controller)
-        print(lead_controller)
-        response = control.evalfr(lead_controller, testing_point)
-        print(response)
-        value_of_K_testing_point = round(1/abs(response),4)
-        magnitud_K = (1/abs(response))
-        print("The magnitud of K corresponding to the test point " + str(testing_point) + " is " + str(magnitud_K))
-        pdf.cell(200, 10,"The magnitud of K corresponding to the test point " + str(testing_point) + " is " + str(magnitud_K), 0, 2, 'L')
+        defficit = 180-angle_G_evaluated_at_testpoint
+        if(defficit <= 90):
+            print("this test point does not belong to the root loccus, the angle defficit is: " + str(defficit) + " degrees or " + str(math.radians(defficit)) + " radians")
+            pdf.cell(200, 10,"this test point does not belong to the root loccus, the angle defficit is: " + str(defficit) + " degrees", 0, 2, 'L')
+            pdf.cell(200, 10," or " + str(math.radians(defficit)) + " radians", 0, 2, 'L')
+            #LEAD CONTROLLER DESIGN
+            new_zero = np.real(testing_point)
+            new_pole = 0
+            angle_new_pole= 90 - defficit
+            if angle_new_pole != 0:
+                distance_pole_zero = (np.imag(testing_point)/math.tan(math.radians(angle_new_pole)))
+                new_pole = new_zero - distance_pole_zero
+            num_and_controler_zero = np.concatenate([[1], [-new_zero]])
+            den_and_controler_pole = np.concatenate([[1], [-new_pole]])
+            if new_pole == 0:
+                den_and_controler_pole = 1
+            if new_zero == 0:
+                num_and_controler_zero = 1
+            lead_controller = control.tf(num_and_controler_zero, den_and_controler_pole)
+            print("LEAD CONTROLLER")
+            print(lead_controller)
+            pdf.set_font('arial', 'B', 12.0)
+            pdf.cell(200, 10, "LEAD CONTROLLER:", 0, 2, 'L')
+            pdf.set_font('arial', '', 11.0)
+            if new_zero!= 0:
+                pdf.cell(200, 10,"the zero of the lead controller is located in " + str(new_zero), 0, 2, 'L')
+            else:
+                pdf.cell(200, 10,"there is no need of an extra zero ", 0, 2, 'L')
+            if new_pole != 0:
+                pdf.cell(200, 10,"the pole of the lead controller is located in" + str(new_pole), 0, 2, 'L')
+            else:
+                pdf.cell(200, 10,"there is no need of an extra pole ", 0, 2, 'L')
+            #find value of K for this testing point
+            controler_and_plant = control.series(G,lead_controller)
+            lead_controller_value = control.evalfr(controler_and_plant, testing_point)
+            value_of_K_lead_controller = 1/abs(lead_controller_value)
+            print("the value of K corresponding to this testing point is " + str(value_of_K_lead_controller))
+            pdf.cell(200, 10,"the value of K corresponding to this testing point is " + str(value_of_K_lead_controller), 0, 2, 'L')
+            #Steady State Error of LEAD CONTROLLER
+            #Lead-Controller evaluated at s=0
+            lead_at_0 = control.evalfr(lead_controller, 0)
+            plant_at_0 = control.evalfr(G, 0)
+            controler_and_plant_evaluated_at_0 = control.evalfr(controler_and_plant, 0)
+            input_to_plant = control.tf([1],[1,0]) #step input
+            s_of_FVT = control.tf([1,0],[1]) #s value of FVT
+            #input_times_s_at_0 = control.evalfr(control.series(input_to_plant,s_of_FVT),0)
+            if(np.isinf(controler_and_plant_evaluated_at_0)):
+                error_ss=0
+            else:
+                error_ss = abs(1/(1+controler_and_plant_evaluated_at_0*value_of_K_lead_controller))
+            print("the error of the steady state value for the lead controller is " + str(error_ss))
+            pdf.cell(200, 10,"the error of the steady state value for the lead controller is " + str(error_ss), 0, 2, 'L')
+            #Lag-lead controller design
+            if(error_ss != 0):    
+                print("LAG-LEAD CONTROLLER")
+                pdf.set_font('arial', 'B', 12.0)
+                pdf.cell(200, 10, "LAG-LEAD CONTROLLER:", 0, 2, 'L')
+                pdf.set_font('arial', '', 11.0)
+                #for an error of 10% less 
+                error_lag_lead_ss = error_ss - 0.1
+                x = Symbol('x')
+                equation = (1/(1+x*lead_at_0*plant_at_0*value_of_K_lead_controller))-error_lag_lead_ss
+                beta = solve(equation,x)
+                print ("The ß of the lag-lead controller is ", str(beta[0]), "for an error of ", str(error_lag_lead_ss))
+                pdf.cell(200, 10,"The ß of the lag-lead controller is " + str(beta[0]) + "for an error of " + str(error_lag_lead_ss), 0, 2, 'L')
+            else: 
+                print("Since the steady state is already 0 with the Lead controller, there is no need for a lag controller")
+                pdf.cell(200, 10,"Since the steady state is already 0 with the Lead controller, there is no need for a lag controller", 0, 2, 'L')
+        else: 
+            print("Defficit is greater than 90º or π rads: we havent seen this case in class (more than one pole/zero is needed")
+            pdf.cell(200, 10,"Defficit is greater than 90º or pi rads: we havent seen this case in class (more than one pole/zero is needed", 0, 2, 'L')
 
 pdf.output('HomeWork_LeadControler_2.pdf', 'F')
+
+        
+
 
 
 
